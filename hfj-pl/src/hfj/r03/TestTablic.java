929dffb774c71561589962d31b1aaccfcb11a2bc
default_git_name <default_git_email>
2012-06-12 15:18:48 +0000

    str. 88: poprawki literowek; cw. test tablic; program dziala poprawnie;

--- hfj-pl/src/hfj/r03/TestTablic.java
+++ hfj-pl/src/hfj/r03/TestTablic.java
@@ -1,32 +1,34 @@
-package hfj.r03;
-class TestTablic{
+package hfj.r03; 
+class TestTablic{ 
 	public static void main(String[] args){
-
-		String wyspy=new String[4];
-
-		wyspy[0]="Bermudy";
-		wyspy[1]="Fiji";
-		wyspy[2]="Azory";
+ 
+		String[] wyspy=new String[4];
+ 
+		wyspy[0]="Bermudy"; 
+		wyspy[1]="Fiji"; 
+		wyspy[2]="Azory"; 
 		wyspy[3]="Kozumel";
-		
+ 
 		int[] indeks=new int[4];
 
-		indeks[0]=1;
-		indeks[1]=3;
-		indeks[2]=0;
+		indeks[0]=1; 
+		indeks[1]=3; 
+		indeks[2]=0; 
 		indeks[3]=2;
 
 		int y=0;
-
-		inT ref;
+		
+		int ref; 
+		
 		while(y<4){
-
-			ref = indeksy[y];
-
-			System.out.print("wyspa");
-			System.out.print(wyspy[ref]);
-
-			y=y+1;
-		} // while
+ 
+			ref = indeks[y];
+ 
+			System.out.print("wyspa = "); 
+			System.out.println(wyspy[ref]);
+		
+			y=y+1; 
+		} // while 
 	} // main 
-}
+} // class
+//str.88: poprawki literowek; cw. test tablic; program dziala poprawnie;
