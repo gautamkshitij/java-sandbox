d660fd95d2c4ec2fabea413c080235ffea11b46a
default_git_name <default_git_email>
2012-06-19 06:14:53 +0000

    str.97: pies tester

---
+++ hfj-pl/src/hfj/r04/Pies.java
@@ -0,0 +1,14 @@
+package hfj.r04;
+class Pies{
+	int wielkosc;
+	String nazwa;
+	void szczekaj(){
+		if(wielkosc>23){
+			System.out.println("Houuu! Houuu!");
+		} else if(wielkosc>6){
+			System.out.println("Chau! Chau!");
+		} else {
+			System.out.println("Hiau! Hiau!");
+		}
+	}
+}
---
+++ hfj-pl/src/hfj/r04/PiesTester.java
@@ -0,0 +1,15 @@
+package hfj.r04;
+class PiesTester{
+	public static void main(String[] args){
+			Pies pierwszy = new Pies();
+			pierwszy.wielkosc=40;
+			Pies drugi = new Pies();
+			drugi.wielkosc=2;
+			Pies trzeci = new Pies();
+			trzeci.wielkosc=8;
+
+			pierwszy.szczekaj();
+			drugi.szczekaj();
+			trzeci.szczekaj();
+	}
+}
