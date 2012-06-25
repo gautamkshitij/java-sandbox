c21d47928378c52ee5c53c4a7be5e7973e5f6cfe
default_git_name <default_git_email>
2012-06-25 14:58:42 +0000

    s.104: hermetyzacja; dokonczenie testu kota

--- hfj-pl/src/hfj/r04/Kot.java
+++ hfj-pl/src/hfj/r04/Kot.java
@@ -1,6 +1,9 @@
 package hfj.r04;
 class Kot{
 	int wysokosc;
+	public int getWysokosc(){
+		return wysokosc;
+	}
 	public void setWysokosc(int w){
 		if(w>9){
 			wysokosc=w;
--- hfj-pl/src/hfj/r04/KotTest.java
+++ hfj-pl/src/hfj/r04/KotTest.java
@@ -1,6 +1,14 @@
 package hfj.r04;
 class KotTest{
 	public static void main(String[] args){
-		// Kot	 FIXME	dokonczyc
+		Kot kot=new Kot();
+		kot.setWysokosc(5);
+		System.out.println("wysokosc kota: "+kot.getWysokosc());
+		kot.setWysokosc(15);
+		System.out.println("wysokosc kota: "+kot.getWysokosc());
+		kot.setWysokosc(7);
+		System.out.println("wysokosc kota: "+kot.getWysokosc());
+		kot.setWysokosc(30);
+		System.out.println("wysokosc kota: "+kot.getWysokosc());
 	}
 }
