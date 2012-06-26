ce4106ff03ccceb57d883bcb042b7bc8ac4fe865
default_git_name <default_git_email>
2012-06-26 15:02:47 +0000

    s.108: domyslne wartosci skladowych

---
+++ hfj-pl/src/hfj/r04/BiednyPies.java
@@ -0,0 +1,11 @@
+package hfj.r04;
+class BiednyPies{
+	private int wielkosc;
+	private String imie;
+	public int getWielkosc(){
+		return wielkosc;
+	}
+	public String getImie(){
+		return imie;
+	}
+}
