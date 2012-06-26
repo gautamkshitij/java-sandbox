7435d28ce09d24cc46a1a2f5150fe942a24b9959
default_git_name <default_git_email>
2012-06-26 05:46:50 +0000

    s.106: hermetyzacja klasy dobry piesek

--- hfj-pl/src/hfj/r04/DobryPiesekTester.java
+++ hfj-pl/src/hfj/r04/DobryPiesekTester.java
@@ -1,6 +1,15 @@
 package hfj.r04;
 class DobryPiesekTester{
 	public static void main(String[] args){
-		// code
+		DobryPiesek pierwszy=new DobryPiesek();
+		pierwszy.setWielkosc(70);
+		DobryPiesek drugi=new DobryPiesek();
+		drugi.setWielkosc(8);
+		System.out.println("pierwszy pies : "+
+			pierwszy.getWielkosc());
+		System.out.println("drugi pies : "+
+			drugi.getWielkosc());
+		pierwszy.szczekaj();
+		drugi.szczekaj();
 	}
 }
