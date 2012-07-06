8cd2b404c8cbd3a1a670014a907a0e4d65521648
default_git_name <default_git_email>
2012-07-06 15:13:01 +0000

    s.115: zagadkowy basen 4 - v1

--- hfj-pl/src/hfj/r04/ZagadkowyBasen4.java
+++ hfj-pl/src/hfj/r04/ZagadkowyBasen4.java
@@ -1,27 +1,27 @@
 package hfj.r04;
 class ZagadkowyBasen4{
 	public static void main(String[] args){
-		// FIXME ...
+		ZagadkowyBasen4b[] obty = new ZagadkowyBasen4b[6];
 		int y=1;
 		int x=0;
 		int wynik=0;
 		while(x<6){
 			// FIXME
-			// FIXME
+			x=x+1;
 			y=y*10;
 			// FIXME
 		} // while
 		x=6;
 		while(x>0){
-			// FIXME
+			x=x-1;
 			wynik=wynik+FIXME;
 		} // while
 		System.out.println("wynik "+wynik);
 	} // main 
 } // class
-class FIXME {
+class ZagadkowyBasen4b {
 	int izm;
-	FIXME FIXME zrobCos(int FIXME){
+	public int zrobCos(int czynnik){
 		if(izm>100){
 			return FIXME;
 		} else {
