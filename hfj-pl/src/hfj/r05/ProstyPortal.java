00a320b39af37e1b39ecc8bc5faa5990dde67b4d
default_git_name <default_git_email>
2012-07-12 06:00:54 +0000

    s.122: prosty portal - starter

---
+++ hfj-pl/src/hfj/r05/ProstyPortal.java
@@ -0,0 +1,12 @@
+package hfj.r05;
+class ProstyPortal{
+	int[] polozeniePol;
+	int iloscTrafien;
+
+	String sprawdz(String pole){
+		return null;
+	}
+	void setPolaPolozenia(int[] ppol){
+		
+	}
+}
---
+++ hfj-pl/src/hfj/r05/ProstyPortalGra.java
@@ -0,0 +1,6 @@
+package hfj.r05;
+class ProstyPortalGra{
+	public static void main(String[] args){
+		// code
+	}
+}
