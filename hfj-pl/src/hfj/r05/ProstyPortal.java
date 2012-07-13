60e4ec829242c420970875e59d3534291c6e8395
default_git_name <default_git_email>
2012-07-13 06:21:09 +0000

    s.128: metoda sprawdz - start implementacji

--- hfj-pl/src/hfj/r05/ProstyPortal.java
+++ hfj-pl/src/hfj/r05/ProstyPortal.java
@@ -3,7 +3,9 @@ class ProstyPortal{
 	int[] polozeniePol;
 	int iloscTrafien;
 
-	String sprawdz(String pole){
+	String sprawdz(String poleString){
+		// konwersja lancucha znakow na liczbe calkowita
+		int pole = Integer.parseInt(poleString);
 		return null;
 	}
 	void setPolaPolozenia(int[] ppol){
