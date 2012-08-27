6c065159ab3e0576b12f827d5087e59420819fbe
default_git_name <default_git_email>
2012-08-27 06:48:01 +0000

    s.131: kod przygotowawczy klasy ProstyPortal gra

--- hfj-pl/src/hfj/r05/ProstyPortalGra.java
+++ hfj-pl/src/hfj/r05/ProstyPortalGra.java
@@ -1,10 +1,47 @@
-package hfj.r05;
-class ProstyPortalGra{
-	public static void main(String[] args){
-		// code
-	}
-}
-package hfj.r05;
+//	METODA public static void main(String[] args){
+
+	// ZADEKLARUJ zmienna typu int
+	// przechowujaca ilosc ruchow gracza,
+	// nadaj jej nazwe iloscRuchow
+		
+	// ZADEKLARUJ zmienna typu ProstyPortal 
+	// i przypisz do niej nowy obiekt 
+		
+	// WYZNACZ liczbe losowa z zakresu 0-4,
+	// ktora okresli poczatek miejsca,
+	// w ktorym zostanie umieszczony portal
+		
+	// OKRESL polozenie portalu (3 sasiadujace komorki) 
+
+	// DOPOKI portal nie zostal zatopiony
+	
+		// POBIERZ	dane wejsciowe wpisane przez
+		// gracza w wierszu polecen
+
+		// SPRAWDZ pole wskazane przez gracza
+		
+		// JESLI pole wskazane przez gracza
+		// wpisuje sie w polozenie portalu
+		
+			// WYSWIETL informacje o tym, ze portal
+			// zostal trafiony
+
+			// zwieksz licznik trafien na obiekcie portal
+
+			// JESLI liczba udanych trafien portalu jest 
+			// rowna ilosci wszystkich jego pol
+	
+				//WYSWIETL informacje o tym, ze portal
+				// zostal zatopiony
+
+				// przerwij sprawdzanie/trafianie przez 
+				// gracza
+
+	// WYSWIETL graczowi informacje o ilosci
+	// wykonanych ruchow
+	
+	// koniec dzialania gry
+
 class ProstyPortalGra{
 	public static void main(String[] args){
 		// code
