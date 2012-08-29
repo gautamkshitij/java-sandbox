cb2e1f6f09bb1a7a9fd005436c50dbfcfbbfcfe6
Lukasz Siwinski <lsiwinski@gmail.com>
2012-08-29 09:37:49 +0200

    deklaracja klasy
    Signed-off-by: Łukasz Siwiński <lsiwinski@gmail.com>

--- hfj-pl/src/hfj/r05/ProstyPortalGra.java
+++ hfj-pl/src/hfj/r05/ProstyPortalGra.java
@@ -1,74 +1,72 @@
 package hfj.r05;
 
-//	METODA public static void main(String[] args){
-
-	// ZADEKLARUJ zmienna typu int
-	// przechowujaca ilosc ruchow gracza,
-	// nadaj jej nazwe iloscRuchow
-		
-	// ZADEKLARUJ zmienna typu ProstyPortal 
-	// i przypisz do niej nowy obiekt 
-		
-	// WYZNACZ liczbe losowa z zakresu 0-4,
-	// ktora okresli poczatek miejsca,
-	// w ktorym zostanie umieszczony portal
-		
-	// UTWORZ tablice 3 liczb calkowitych, z ktorych
-	// pierwsza bedzie wartoscia wygenerowana losowo
-	// druga wartoscia o 1 wieksza, a 3 wartoscia
-	// wieksza o 2 (np. 2,3,4)
-	
-	// OKRESL polozenie portalu (3 sasiadujace komorki) 
-	
-	// ZADEKLARUJ zmienna logiczna reprezentujaca stan 
-	// gry i nada jej nazwe czyIstnieje. PRZYPISZ tej 
-	// zmiennej wartosc poczatkowa true
-
-	// DOPOKI portal nie zostal zatopiony 
-	// (czyIstnieje==true):
-	
-		// POBIERZ	dane wejsciowe wpisane przez
-		// gracza w wierszu polecen
-
-		// SPRAWDZ pole wskazane przez gracza 
-		// wywolaj metode sprawdz obiektu 
-		// klasy ProstyPortal
-
-		// INKREMENTUJ zmienna iloscRuchow 
-
-		// JESLI pole wskazane przez gracza
-		// wpisuje sie w polozenie portalu
-		
-			// WYSWIETL informacje o tym, ze portal
-			// zostal trafiony
-
-			// zwieksz licznik trafien na obiekcie portal
-
-			// JESLI liczba udanych trafien portalu jest 
-			// rowna ilosci wszystkich jego pol
-			// to portal jest zatopiony (sprawdzenie 
-			// czy portal zostal zatopiony)
-	
-				// PRZYPISZ zmiennej czyIstnieje wartosc false 
-				// (co oznacza, ze petla nie zostanie 
-				// juz wiecej wykonana
-
-				// WYSWIETL ilosc ruchow gracza oraz 
-				// informacje o tym, ze portal
-				// zostal zatopiony
-
-				// przerwij sprawdzanie/trafianie przez 
-				// gracza
-
-			// koniec JESLI
-		// koniec JESLI
-	// koniec DOPOKI
-// koniec METODY
-
-// koniec dzialania gry
-
 class ProstyPortalGra{
 	public static void main(String[] args){
-		// code
+//		METODA public static void main(String[] args){
+
+		// ZADEKLARUJ zmienna typu int
+		// przechowujaca ilosc ruchow gracza,
+		// nadaj jej nazwe iloscRuchow
+			
+		// ZADEKLARUJ zmienna typu ProstyPortal 
+		// i przypisz do niej nowy obiekt 
+			
+		// WYZNACZ liczbe losowa z zakresu 0-4,
+		// ktora okresli poczatek miejsca,
+		// w ktorym zostanie umieszczony portal
+			
+		// UTWORZ tablice 3 liczb calkowitych, z ktorych
+		// pierwsza bedzie wartoscia wygenerowana losowo
+		// druga wartoscia o 1 wieksza, a 3 wartoscia
+		// wieksza o 2 (np. 2,3,4)
+		
+		// OKRESL polozenie portalu (3 sasiadujace komorki) 
+		
+		// ZADEKLARUJ zmienna logiczna reprezentujaca stan 
+		// gry i nada jej nazwe czyIstnieje. PRZYPISZ tej 
+		// zmiennej wartosc poczatkowa true
+
+		// DOPOKI portal nie zostal zatopiony 
+		// (czyIstnieje==true):
+		
+			// POBIERZ	dane wejsciowe wpisane przez
+			// gracza w wierszu polecen
+
+			// SPRAWDZ pole wskazane przez gracza 
+			// wywolaj metode sprawdz obiektu 
+			// klasy ProstyPortal
+
+			// INKREMENTUJ zmienna iloscRuchow 
+
+			// JESLI pole wskazane przez gracza
+			// wpisuje sie w polozenie portalu
+			
+				// WYSWIETL informacje o tym, ze portal
+				// zostal trafiony
+
+				// zwieksz licznik trafien na obiekcie portal
+
+				// JESLI liczba udanych trafien portalu jest 
+				// rowna ilosci wszystkich jego pol
+				// to portal jest zatopiony (sprawdzenie 
+				// czy portal zostal zatopiony)
+		
+					// PRZYPISZ zmiennej czyIstnieje wartosc false 
+					// (co oznacza, ze petla nie zostanie 
+					// juz wiecej wykonana
+
+					// WYSWIETL ilosc ruchow gracza oraz 
+					// informacje o tym, ze portal
+					// zostal zatopiony
+
+					// przerwij sprawdzanie/trafianie przez 
+					// gracza
+
+				// koniec JESLI
+			// koniec JESLI
+		// koniec DOPOKI
+//	 koniec METODY
+
+//	 koniec dzialania gry
 	}
 }
