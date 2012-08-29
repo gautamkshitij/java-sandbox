0635c97e42901960ebf1d6b9dcd827ad9771b4e4
Lukasz Siwinski <lsiwinski@gmail.com>
2012-08-29 09:52:31 +0200

    p.134: zmiana kodu przygotowawczego w kod napisany w jezyku java
    Signed-off-by: Łukasz Siwiński <lsiwinski@gmail.com>

--- hfj-pl/src/hfj/r05/ProstyPortalGra.java
+++ hfj-pl/src/hfj/r05/ProstyPortalGra.java
@@ -2,71 +2,33 @@ package hfj.r05;
 
 class ProstyPortalGra{
 	public static void main(String[] args){
-//		METODA public static void main(String[] args){
 
-		// ZADEKLARUJ zmienna typu int
-		// przechowujaca ilosc ruchow gracza,
-		// nadaj jej nazwe iloscRuchow
-			
-		// ZADEKLARUJ zmienna typu ProstyPortal 
-		// i przypisz do niej nowy obiekt 
-			
-		// WYZNACZ liczbe losowa z zakresu 0-4,
-		// ktora okresli poczatek miejsca,
-		// w ktorym zostanie umieszczony portal
-			
-		// UTWORZ tablice 3 liczb calkowitych, z ktorych
-		// pierwsza bedzie wartoscia wygenerowana losowo
-		// druga wartoscia o 1 wieksza, a 3 wartoscia
-		// wieksza o 2 (np. 2,3,4)
+		int iloscRuchow = 0;
+
+		ProstyPortal portal = new ProstyPortal();
+
+		int liczbaLosowa = (int) (Math.random()*5);
+
+		int[] polaPolozenia = { liczbaLosowa, liczbaLosowa+1, liczbaLosowa+2};  
 		
-		// OKRESL polozenie portalu (3 sasiadujace komorki) 
+		portal.setPolaPolozenia(polaPolozenia); 
 		
-		// ZADEKLARUJ zmienna logiczna reprezentujaca stan 
-		// gry i nada jej nazwe czyIstnieje. PRZYPISZ tej 
-		// zmiennej wartosc poczatkowa true
+		boolean czyIstnieje = true; 
 
-		// DOPOKI portal nie zostal zatopiony 
-		// (czyIstnieje==true):
+		while(czyIstnieje) {
 		
-			// POBIERZ	dane wejsciowe wpisane przez
-			// gracza w wierszu polecen
+			String podanePole = PomocnikGry.pobierzDaneWejsciowe("Podaj liczbe");
 
-			// SPRAWDZ pole wskazane przez gracza 
-			// wywolaj metode sprawdz obiektu 
-			// klasy ProstyPortal
+			String wynik = portal.sprawdz(podanePole);
 
-			// INKREMENTUJ zmienna iloscRuchow 
+			iloscRuchow++; 
 
-			// JESLI pole wskazane przez gracza
-			// wpisuje sie w polozenie portalu
-			
-				// WYSWIETL informacje o tym, ze portal
-				// zostal trafiony
+			if("zatopiony".equals(wynik)){// JESLI liczba udanych trafien portalu jest 
+	
+				czyIstnieje = false;
 
-				// zwieksz licznik trafien na obiekcie portal
-
-				// JESLI liczba udanych trafien portalu jest 
-				// rowna ilosci wszystkich jego pol
-				// to portal jest zatopiony (sprawdzenie 
-				// czy portal zostal zatopiony)
-		
-					// PRZYPISZ zmiennej czyIstnieje wartosc false 
-					// (co oznacza, ze petla nie zostanie 
-					// juz wiecej wykonana
-
-					// WYSWIETL ilosc ruchow gracza oraz 
-					// informacje o tym, ze portal
-					// zostal zatopiony
-
-					// przerwij sprawdzanie/trafianie przez 
-					// gracza
-
-				// koniec JESLI
-			// koniec JESLI
-		// koniec DOPOKI
-//	 koniec METODY
-
-//	 koniec dzialania gry
-	}
+				System.out.println("Ilosc Ruchow: " + iloscRuchow);
+			} // if
+		} // while 
+	} // main
 }
