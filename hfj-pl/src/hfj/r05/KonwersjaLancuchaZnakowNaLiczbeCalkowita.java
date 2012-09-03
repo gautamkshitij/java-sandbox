d13d1003aa11451f6e6a89f1b2a4ebd504c6a4f2
Łukasz Siwiński <lsiwinski@gmail.com>
2012-09-03 22:34:07 +0200

    s.140: konwersja lancucha znakow na liczbe calkowita, taka instrukcja wywola blad - 'Incompatible operand types int and String'
    Signed-off-by: Łukasz Siwiński <lsiwinski@gmail.com>

---
+++ hfj-pl/src/hfj/r05/KonwersjaLancuchaZnakowNaLiczbeCalkowita.java
@@ -0,0 +1,11 @@
+package hfj.r05;
+
+public class KonwersjaLancuchaZnakowNaLiczbeCalkowita {
+	public static void main(String[] args) {
+		String liczba = "2";
+		int x = 2;
+		if (x==liczba){ // kabooooom!!!!
+			
+		}
+	}
+}
