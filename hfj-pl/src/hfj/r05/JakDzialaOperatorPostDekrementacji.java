5a44cf916fa77fa17f2c830db78716807f8f754b
Lukasz Siwinski <lsiwinski@gmail.com>
2012-09-03 10:34:22 +0200

    s.139: jak dziala operator post dekrementacji
    Signed-off-by: Łukasz Siwiński <lsiwinski@gmail.com>

---
+++ hfj-pl/src/hfj/r05/JakDzialaOperatorPostDekrementacji.java
@@ -0,0 +1,12 @@
+package hfj.r05;
+
+public class JakDzialaOperatorPostDekrementacji {
+	public static void main(String[] args) {
+		int x = 0;
+		System.out.println("1. " + x);
+		x--; // odpowiada: x = x - 1;
+		System.out.println("2. " + x);
+		System.out.println("3. " + x--);
+		System.out.println("4. " + x);
+	}
+}
