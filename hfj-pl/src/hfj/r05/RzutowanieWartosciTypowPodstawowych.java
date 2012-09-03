2b5291be18b5ffe1d1020f379525bdadc1c30c12
Łukasz Siwiński <lsiwinski@gmail.com>
2012-09-03 22:29:44 +0200

    s.140: rzutowanie wartosci zmiennoprzecinkowej (float) na typ calkowity (int)
    Signed-off-by: Łukasz Siwiński <lsiwinski@gmail.com>

--- hfj-pl/src/hfj/r05/RzutowanieWartosciTypowPodstawowych.java
+++ hfj-pl/src/hfj/r05/RzutowanieWartosciTypowPodstawowych.java
@@ -3,8 +3,14 @@ package hfj.r05;
 public class RzutowanieWartosciTypowPodstawowych {
 
 	public static void main(String[] args) {
-	long y = 40002; // ta liczba przekracza zakres 16-bitowych wartosci
+		long y = 40002; // ta liczba przekracza zakres 16-bitowych wartosci
                         // typu short}
         short x = (short) y; // x ma wartosc -25534
         System.out.println(x);
+        
+        float f = 3.14f;
+		int z = (int)f;
+        System.out.println("f: " + f);
+        System.out.println("z: " + z); // z bedzie miec wartosc 3
+	}
 }
