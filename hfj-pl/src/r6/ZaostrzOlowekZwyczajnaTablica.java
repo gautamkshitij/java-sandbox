a676e3969a8cf5b942361938d1df002f53209074
Lukasz Siwinski <lsiwinski@gmail.com>
2012-09-10 10:34:40 +0200

    s.158: cw. zaostrz olowek - wersja z ksiazki

--- hfj-pl/src/r6/ZaostrzOlowekZwyczajnaTablica.java
+++ hfj-pl/src/r6/ZaostrzOlowekZwyczajnaTablica.java
@@ -3,21 +3,18 @@ package r6;
 public class ZaostrzOlowekZwyczajnaTablica {
 	public static void main(String[] args) {
 		String[] mojaTab = new String[2];
-//		int ilosc = 0;
 		String a = new String("hoohoo");
 		mojaTab[0] = a;
-//		ilosc++;
 		String b = new String("Zaba");
 		mojaTab[1] = b;
-//		ilosc++;
 		int ilosc = mojaTab.length;
-		Object o = mojaTab[1];
+		String o = mojaTab[1];
 		mojaTab[1] = null;
 		boolean czyJest = false;
-		for(int i=0; i<ilosc; i++){
-			String el  = mojaTab[i];
-			if(b.equals(el)){
+		for(int i=0; i<ilosc; i++){ // tej linijki nie bylo w ksiazce (blad w druku)
+			if(b.equals(mojaTab[i])){
 				czyJest=true;
+				break;
 			}
 		}
 		System.out.println("czyJest: " + czyJest);
