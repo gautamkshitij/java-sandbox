a7f1a4651bcb59af089885ab9c5ea0fb6e1d0cd7
Lukasz Siwinski <lsiwinski@gmail.com>
2012-09-10 11:03:29 +0200

    s.161: nowa poprawiona wersja klasy ProstyPortal (wykorzystujaca mozliwosci ArrayList)

--- hfj-pl/src/r6/ProstyPortal.java
+++ hfj-pl/src/r6/ProstyPortal.java
@@ -1,26 +1,28 @@
 package r6;
+
+import java.util.ArrayList;
+
 class ProstyPortal{
 
-	int[] polozeniePol;
-	int iloscTrafien;
+	private ArrayList<Object> polozeniePol; // zmiana na ArrayList
+	// int iloscTrafien; (juz niepotrzebne)
 
-	void setPolaPolozenia(int[] ppol){
+	void setPolaPolozenia(ArrayList<Object> ppol){ // zmiana na ArrayList
 		polozeniePol=ppol;
 	}
 
-	String sprawdz(String poleString){
-		int pole = Integer.parseInt(poleString);
+	String sprawdz(String ruch){
+		int pole = Integer.parseInt(ruch);
 		String wynik="pudlo";
-		for(int i=0; i<polozeniePol.length; i++){
-			if(pole==polozeniePol[i]){
+		int indeks = polozeniePol.indexOf(pole);
+		if(indeks>=0){ // jesli indeks ma wartosc wieksza/rowna 0 to pole jest obecje w tablicy
+			polozeniePol.remove(indeks);
+			if(polozeniePol.isEmpty()){ // jesli tabl. pol polozenia jest pusta to mamy ostatnie trafienie
+				wynik="zatopiony";
+			} else {
 				wynik="trafiony";
-				iloscTrafien++;
-				break;
-			}
-		}
-		if(iloscTrafien==polozeniePol.length){
-			wynik="zatopiony";
-		}
+			} // koniec if
+		} // koniec if
 		System.out.println(wynik);
 		return wynik;
 	} // koniec metody
