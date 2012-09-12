400db7be4c24f09c262e13bc8975929d8cc44772
Łukasz Siwiński <lsiwinski@gmail.com>
2012-09-12 23:21:56 +0200

    s.167: kod testujacy klase PortalGraMax

---
+++ hfj-pl/src/r6/PortalGraMaxTest.java
@@ -0,0 +1,47 @@
+package r6;
+
+public class PortalGraMaxTest {
+
+	public static void main(String[] args) {
+		PortalGraMaxTest test = new PortalGraMaxTest();
+		test.przygotujGreTest();
+		test.rozpocznijGreTest();
+		test.sprawdzRuchGraczaTest();
+		test.zakonczGreTest();
+	}
+	
+	void przygotujGreTest(){
+		PortalGraMax gra = new PortalGraMax();
+		gra.przygotujGre();
+		if(gra.listaPortali==null) pokaBlad();
+		if(gra.listaPortali!=3) pokaBlad();
+	}
+	
+	void rozpocznijGreTest(){
+		// tej metody chyba nie da sie przetestowac
+	}
+	
+	void sprawdzRuchGraczaTest(){
+		PortalGraMax gra = new PortalGraMax();
+		gra.przygotujGre();
+		String[] abc = new String[] {"a","b","c","d","e","f","g"};
+		for (int i = 0; i < abc.length; i++) {
+			String litera = abc[i];
+			for (int j = 0; j < 8; j++) {
+				String liczba = String.valueOf(j);
+				String wpisanePole = litera + liczba;
+				gra.sprawdzRuchGracza(wpisanePole);
+			}
+		}
+		if(!gra.listaPortali.isEmpty()) pokaBlad();
+		if(gra.iloscRuchow!=64) pokaBlad();
+	}
+	
+	void zakonczGreTest(){
+		// tej metody chyba teý nie da sie przetestowac
+	}
+
+	private void pokaBlad() {
+		System.out.println("blad");
+	}
+}
