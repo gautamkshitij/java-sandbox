17a976cc602194957a6d74d6e322e83efc926327
Łukasz Siwiński <lsiwinski@gmail.com>
2012-09-21 00:16:25 +0200

    s.177: druga metoda importowania/korzystania z klas dostarczonych w Java API

---
+++ hfj-pl/src/r6/ImportTest2.java
@@ -0,0 +1,21 @@
+package r6;
+
+import java.util.ArrayList;
+
+public class ImportTest2 {
+
+	// a tutaj mamy inna metode wykorzystywania klas
+	// ze standardowej biblioteki Java API
+	
+	
+	java.util.ArrayList listaImion = new java.util.ArrayList();
+	
+	public void jazda(java.util.ArrayList liste){
+		
+	}
+	
+	public java.util.ArrayList metodaPomocnicza(){
+		return new java.util.ArrayList();
+	}
+	
+}
