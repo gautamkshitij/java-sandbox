0d508fd5faeb6c50700dcde917a9201022b47642
Łukasz Siwiński <lsiwinski@gmail.com>
2012-09-26 00:31:31 +0200

    zamiana kolejnosci metod

--- hfj-pl/src/r6/ArrayList6.java
+++ hfj-pl/src/r6/ArrayList6.java
@@ -4,6 +4,39 @@ import java.util.*;
 
 public class ArrayList6 {
 
+	public static void main(String[] args){
+		ArrayList<Object> a = new ArrayList<Object>();
+		
+		if(a.contains("dwa")){
+			a.add("2.2");
+		}
+		
+		a.add(0,"zero");
+		a.add(1,"jeden");
+		
+		a.add(2,"dwa");
+		a.add(3,"trzy");
+		
+		wyswietlWszystko(a);
+		
+		
+		if(a.contains("trzy")){
+			a.add("cztery");
+		}
+		
+		a.remove(2);
+		
+		wyswietlWszystko(a);
+		
+		if(a.indexOf("cztery")!=4){
+			a.add(4, "4.2");
+		}
+		
+		wyswietlWszystko(a);
+		
+		wyswietlWszystko(a);
+	}
+	
 	public static void wyswietlWszystko(ArrayList al){
 		for(int z=0; z<al.size();z++){
 			
@@ -13,37 +46,5 @@ public class ArrayList6 {
 		}
 
 		System.out.println(" ");
-	}
-	public static void main(String[] args){
-		ArrayList<Object> a = new ArrayList<Object>();
-
-		if(a.contains("dwa")){
-			a.add("2.2");
-		}
-
-		a.add(0,"zero");
-		a.add(1,"jeden");
-		
-		a.add(2,"dwa");
-		a.add(3,"trzy");
-		
-		wyswietlWszystko(a);
-
-		
-		if(a.contains("trzy")){
-			a.add("cztery");
-		}
-		
-		a.remove(2);
-
-		wyswietlWszystko(a);
-
-		if(a.indexOf("cztery")!=4){
-			a.add(4, "4.2");
-		}
-		
-		wyswietlWszystko(a);
-
-		wyswietlWszystko(a);
 	}
 }
