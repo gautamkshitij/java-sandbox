30d404c5e52ca77029a445d3ff42c87276a8f1be
Łukasz Siwiński <lsiwinski@gmail.com>
2012-10-10 22:54:36 +0200

    s.200: slowo kluczowe 'super' - umozliwia wywolanie przeslonietej metody z klasy rodzica

--- hfj-pl/src/r7/Psowate.java
+++ hfj-pl/src/r7/Psowate.java
@@ -3,6 +3,11 @@ package r7;
 public class Psowate extends Zwierze{
 
 	public void wedruj() {
+		
+		// ta instrukcja wywoluje odziedziczona wersje metody wedruj().
+		// a potem wracamy, aby wykonac wlasny kod
+		super.wedruj();
+		
 		System.out.println("wedruj() z klasy Psowate");
 	}
 }
