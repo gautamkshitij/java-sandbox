db0686960b59f6a1ca7e21bd500e4c485d1c1544
Łukasz Siwiński <lsiwinski@gmail.com>
2012-10-11 19:27:04 +0200

    s.205: cd. wstepu do polimorfizmu - zarowno obiekt jak i odwolanie sa innych typow, a mimo wszystko program dalej dziala tak samo (wykorzystywane sa metody klasy Pies, a nie Zwierze, pomimo, ze zmienna referencyjna jest typu Zwierze)

--- hfj-pl/src/r7/WstepDoPoliformizmuDemo.java
+++ hfj-pl/src/r7/WstepDoPoliformizmuDemo.java
@@ -5,7 +5,7 @@ public class WstepDoPoliformizmuDemo {
 	public static void main(String[] args) {
 		
 		// normalne stworzenie obiektu i przypisanie do zmiennej tego samego typu
-		Pies mojPies = new Pies();
+		Zwierze mojPies = new Pies();
 		mojPies.jedz();
 		mojPies.halasuj();
 	}
