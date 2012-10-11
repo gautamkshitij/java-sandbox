a9c1bb1bae08523ef61ed87ae62c5a6c2b7b6ce2
Łukasz Siwiński <lsiwinski@gmail.com>
2012-10-11 19:24:28 +0200

    s.204: wstep do polimorfizmu - normalne stworzenie obiektu klasy Pies i przypisanie go do zmiennej referencyjnej tego samego typu (Pies)

---
+++ hfj-pl/src/r7/Pies.java
@@ -0,0 +1,12 @@
+package r7;
+
+public class Pies extends Psowate {
+
+	public void halasuj() {
+		System.out.println("halasuj() z klasy Pies");
+	}
+	
+	public void jedz() {
+		System.out.println("jedz() z klasy Pies");
+	}
+}
---
+++ hfj-pl/src/r7/WstepDoPoliformizmuDemo.java
@@ -0,0 +1,12 @@
+package r7;
+
+public class WstepDoPoliformizmuDemo {
+
+	public static void main(String[] args) {
+		
+		// normalne stworzenie obiektu i przypisanie do zmiennej tego samego typu
+		Pies mojPies = new Pies();
+		mojPies.jedz();
+		mojPies.halasuj();
+	}
+}
