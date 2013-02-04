11f01e640d3890eed09af8c12dc0fb3f21228449
Łukasz Siwiński <lsiwinski@gmail.com>
2013-02-05 00:26:06 +0100

    and this is the full code, that enable to change final fields values

--- try-it-out/src/jreflection/Doh.java
+++ try-it-out/src/jreflection/Doh.java
@@ -5,6 +5,33 @@ import java.lang.reflect.Modifier;
 
 public class Doh {
 
+	public static void main(String[] args) throws Exception {
+		Person p = new Person("Piter");
+		System.out.println(p.getName());
+		System.out.println(Person.DEFAULT);
+
+		setName(p, "New Name");
+		setDefault("Bob");
+		
+		System.out.println(p.getName());
+		System.out.println(Person.DEFAULT);
+	}
+	
+	private static void setDefault(String newDefault) throws Exception {
+		Field staticField = Person.class.getDeclaredField("DEFAULT");
+		setValue(null, staticField, newDefault);
+	}
+
+	private static void setName(Person p, String newName) throws Exception {
+		Field field = Person.class.getDeclaredField("name");
+		setValue(p, field, newName);
+	}
+
+	private static void setValue(Object owner, Field field, Object value) throws Exception {
+		makeModifiable(field);
+		field.set(owner, value);
+	}
+
 	/**
 	 * force field to be accessible & modifiable
 	 * @param fieldName
--- try-it-out/src/jreflection/Person.java
+++ try-it-out/src/jreflection/Person.java
@@ -5,6 +5,8 @@ package jreflection;
  */
 class Person {
 
+	// avoid inlining by calling newString("unnamed")
+	public static final String DEFAULT = new String("unnamed");
 	private final String name;
 
 	public Person(String name) {
