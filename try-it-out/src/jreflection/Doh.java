5d6c4567e627e1213fe44e9ff2493c317be232be
Łukasz Siwiński <lsiwinski@gmail.com>
2013-02-05 00:30:57 +0100

    added link to blog entry when I've found this snippet code & changed throws declarations

--- try-it-out/src/jreflection/Doh.java
+++ try-it-out/src/jreflection/Doh.java
@@ -3,9 +3,12 @@ package jreflection;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
+/**
+ * @see http://zarnekow.blogspot.ch/2013/01/java-hacks-changing-final-fields.html?m=1
+ */
 public class Doh {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException  {
 		Person p = new Person("Piter");
 		System.out.println(p.getName());
 		System.out.println(Person.DEFAULT);
@@ -17,17 +20,17 @@ public class Doh {
 		System.out.println(Person.DEFAULT);
 	}
 	
-	private static void setDefault(String newDefault) throws Exception {
+	private static void setDefault(String newDefault) throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException  {
 		Field staticField = Person.class.getDeclaredField("DEFAULT");
 		setValue(null, staticField, newDefault);
 	}
 
-	private static void setName(Person p, String newName) throws Exception {
+	private static void setName(Person p, String newName) throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException  {
 		Field field = Person.class.getDeclaredField("name");
 		setValue(p, field, newName);
 	}
 
-	private static void setValue(Object owner, Field field, Object value) throws Exception {
+	private static void setValue(Object owner, Field field, Object value) throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException  {
 		makeModifiable(field);
 		field.set(owner, value);
 	}
@@ -35,8 +38,12 @@ public class Doh {
 	/**
 	 * force field to be accessible & modifiable
 	 * @param fieldName
+	 * @throws NoSuchFieldException 
+	 * @throws SecurityException 
+	 * @throws IllegalAccessException 
+	 * @throws IllegalArgumentException 
 	 */
-	protected static void makeModifiable(Field field) throws Exception {
+	protected static void makeModifiable(Field field) throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException  {
 		field.setAccessible(true);
 		int modifiers = field.getModifiers(); 
 		Field modifierField = field.getClass().getDeclaredField("modifiers");
