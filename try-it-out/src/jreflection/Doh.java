98022fd1ae5181be539039426bf65badb61788f3
Łukasz Siwiński <lsiwinski@gmail.com>
2013-02-05 00:10:51 +0100

    now it should be okay to alter field value

--- try-it-out/src/jreflection/Doh.java
+++ try-it-out/src/jreflection/Doh.java
@@ -1,5 +1,20 @@
 package jreflection;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 public class Doh {
 
+	/**
+	 * force field to be accessible & modifiable
+	 * @param fieldName
+	 */
+	protected static void makeModifiable(Field field) throws Exception {
+		field.setAccessible(true);
+		int modifiers = field.getModifiers(); 
+		Field modifierField = field.getClass().getDeclaredField("modifiers");
+		modifiers = modifiers & ~Modifier.FINAL;
+		modifierField.setAccessible(true);
+		modifierField.setInt(field, modifiers);
+	}
 }
